5b90548125ef434fa88ae61c8cc504aed1198767
0500920-0 <tomfongmi@gmail.com>
2021-12-21 16:34:29 +0800

    Add serving rules, arrange buttons, etc.

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@ type ScoreDict = {
   score: number,
   isDeuce: boolean,
   hasRightToServe: boolean,
+  isChangingSide: boolean,
 };
 
 // the 0th one is on the left, the 1st one is on the right
@@ -13,16 +14,18 @@ type ScoreBoard = [ScoreDict, ScoreDict];
 
 const initScoreBoard: ScoreBoard = [
   {
-    teamName: "ひだり",
+    teamName: "",
     score: 0,
     isDeuce: false,
     hasRightToServe: false,
+    isChangingSide: false,
   },
   {
-    teamName: "みぎ",
+    teamName: "",
     score: 0,
     isDeuce: false,
     hasRightToServe: false,
+    isChangingSide: false,
   },
 ];
 
@@ -33,30 +36,33 @@ function useScoreBoard(initScoreBoard: ScoreBoard) {
     switch (action.type) {
       case 'L+':
         return [
-          { ...state[0], hasRightToServe: true, score: state[0].score+1 },
+          { ...state[0], hasRightToServe: true, score: state[0].score+1, isChangingSide: state[0].hasRightToServe },
           { ...state[1], hasRightToServe: false },
         ] as ScoreBoard;
       case 'R+':
         return [
           { ...state[0], hasRightToServe: false },
-          { ...state[1], hasRightToServe: true, score: state[1].score+1 },
+          { ...state[1], hasRightToServe: true, score: state[1].score+1, isChangingSide: state[1].hasRightToServe },
         ] as ScoreBoard;
 
       case 'L-':
         if (state[0].score > 0) return [
-          { ...state[0], score: state[0].score-1 },
-          state[1],
+          { ...state[0], score: state[0].score-1, hasRightToServe: false },
+          { ...state[1], hasRightToServe: false },
         ] as ScoreBoard;
         else return state;
       case 'R-':
         if (state[1].score > 0) return [
-          state[0],
-          { ...state[1], score: state[1].score-1 },
+          { ...state[0], hasRightToServe: false },
+          { ...state[1], score: state[1].score-1, hasRightToServe: false },
         ] as ScoreBoard;
         else return state;
 
       case 'ChangeSide':
-        return [state[1], state[0]] as ScoreBoard;
+        return [
+          { ...state[1], isChangingSide: false },
+          { ...state[0], isChangingSide: false }
+        ] as ScoreBoard;
       case 'LChangeName':
         return [
           { ...state[0], teamName: action.payload },
@@ -89,15 +95,52 @@ function useScoreBoard(initScoreBoard: ScoreBoard) {
 
 export function App() {
   const { left, right, dispatch } = useScoreBoard(initScoreBoard);
+
+  const handleFullscreenButtonClick = () => {
+    if (!document.fullscreenElement) document.documentElement.requestFullscreen();
+    else document?.exitFullscreen();
+    return () => { document?.exitFullscreen(); };
+  };
+
+  function handleScoreChange({ isAdding, isOnTheRight }: { isAdding: boolean, isOnTheRight: boolean }) {
+      let type = '';
+      if (isOnTheRight) type += 'R';
+      else type += 'L';
+
+      if (isAdding) type += '+';
+      else type += '-';
+
+      return () => { dispatch({ type }); };
+  }
+
   return (
     <>
-      <Score teamName={left.teamName} score={left.score} dispatch={dispatch}/>
+      <Score
+        teamName={left.teamName}
+        score={left.score}
+        hasRightToServe={left.hasRightToServe}
+        isChangingSide={left.isChangingSide}
+        onNameChange={(name) => { dispatch({ type: 'LChangeName', payload: name }); }}
+        onScoreChange={() => { dispatch({ type: 'L+' }); }}
+      />
       <nav>
-        <span>測試版</span>
+        <span>計分板</span>
+        <button onClick={handleFullscreenButtonClick}>全螢幕</button>
         <button onClick={() => dispatch({ type: 'Zero' })}>歸零</button>
         <button onClick={() => dispatch({ type: 'ChangeSide' })}>換邊</button>
+        <div>
+          <button onClick={() => dispatch({ type: 'L-' })}>⬅️減分</button>
+          <button onClick={() => dispatch({ type: 'R-' })}>減分➡️</button>
+        </div>
       </nav>
-      <Score teamName={right.teamName} score={right.score} dispatch={dispatch} isOnTheRight/>
+      <Score
+        teamName={right.teamName}
+        score={right.score}
+        hasRightToServe={right.hasRightToServe}
+        isChangingSide={right.isChangingSide}
+        onNameChange={(name) => { dispatch({ type: 'RChangeName', payload: name }); }}
+        onScoreChange={() => { dispatch({ type: 'R+' }); }}
+      />
     </>
   )
 }
--- src/score.tsx
+++ src/score.tsx
@@ -1,22 +1,33 @@
-import type { scoreBoardAction } from "./app"
+import type { scoreBoardAction } from "./app";
 
 export type ScoreProps = {
     teamName: string,
     score: number,
-    isOnTheRight?: boolean,
-    dispatch(action: scoreBoardAction): void,
+    // isOnTheRight?: boolean,
+    hasRightToServe: boolean,
+    isChangingSide: boolean,
+    // dispatch(action: scoreBoardAction): void,
+    onNameChange(name: string): void,
+    onScoreChange(): void,
 };
 
-export function Score({ teamName, score, isOnTheRight = false, dispatch }: ScoreProps) {
+export function Score({ teamName, score, hasRightToServe, isChangingSide, onNameChange, onScoreChange }: ScoreProps) {
+    function handleNameChange(evt: any) { /* TODO: I don't know what the type is here (with Preact)  */
+        evt.preventDefault();
+        if (evt.target instanceof HTMLInputElement)
+            // dispatch({ type: `${isOnTheRight?'R':'L'}ChangeName`, payload: evt?.target?.value });
+            onNameChange(evt?.target?.value);
+    }
     return <div class="score">
         <header>
             <input
             value={teamName}
-            onInput={(evt) => dispatch({ type: `${isOnTheRight?'R':'L'}ChangeName`, payload: evt?.target.value })}
+            onInput={handleNameChange}
+            placeholder="請輸入名字……"
             />
         </header>
-        <main>{score}</main>
-        <footer>
+        <main onClick={onScoreChange} className={isChangingSide ? 'change-side' : hasRightToServe ? 'service' : ''}>{score}</main>
+        {/* <footer>
         {
             isOnTheRight ?
             <>
@@ -28,6 +39,6 @@ export function Score({ teamName, score, isOnTheRight = false, dispatch }: Score
                 <button onClick={() => dispatch({ type: 'L-' })}>-減分</button>
             </>
         }
-        </footer>
+        </footer> */}
   </div>
 }
